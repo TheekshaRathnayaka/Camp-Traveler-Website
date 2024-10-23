6188f44274cce8d80f23623f0451960d50f1a31e
TheekshaRathnayaka <jrntheeksha@std.appsc.sab.ac.lk>
2024-10-23 12:47:44 +0530

    Add images to public folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,11 @@ export default function RootLayout({
 
       <body>
         <Navbar />
-        {children}
+
+        <main className="relative overflow-hidden">
+          {children}
+        </main>
+        
         <Footer />
       </body>
 
