0030cb5a4da2a980aa544f60b62e0bbc175343cb
TheekshaRathnayaka <jrntheeksha@std.appsc.sab.ac.lk>
2024-10-23 16:23:18 +0530

    Create button component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,8 @@
+import { NAV_LINKS } from "@/constants"
+import { link } from "fs"
 import Image from "next/image"
 import Link from "next/link"
+import Button from "./Button"
 
 const Navbar = () => {
   return (
@@ -11,8 +14,22 @@ const Navbar = () => {
         </Link>
 
         <ul className="hidden h-full gap-12 lg:flex">
-            
+            {NAV_LINKS.map((link) => (
+                <Link href={link.href} key={link.key} className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold">
+                    {link.label}
+                </Link>
+            ))}
         </ul>
+
+        <div className="lg:flexCenter hidden">
+            <Button 
+               type="button"
+               title="Login"
+               icon="/user.svg"
+               variant="btn_dark_green"
+            />
+        </div>
+        
     </nav>
   )
 }
