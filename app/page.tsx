3fad9cab19cbe77005a0052e23b948928f538b69
TheekshaRathnayaka <jrntheeksha@std.appsc.sab.ac.lk>
2024-10-24 16:10:24 +0530

    Design Hero section with images

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,13 @@ import Features from "@/components/Features";
 import GetApp from "@/components/GetApp";
 import Guide from "@/components/Guide";
 import Hero from "@/components/Hero";
-import Image from "next/image";
+
 
 export default function Home() {
   return (
 
     <>
-    
+
       <Hero />
       <Camp />
       <Guide />
