a2b107f9834aa9e750be834ef9a6072bb774950b
TheekshaRathnayaka <jrntheeksha@std.appsc.sab.ac.lk>
2024-11-03 23:37:28 +0530

    Start guide section

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -1,10 +1,13 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Guide = () => {
   return (
-    <div>
-      
-    </div>
+    <section className="flexCenter flex-col">
+      <div className="padding-container max-container w-full pb-24">
+        <Image src="/camp.svg" alt="camp" width={50} height={50} />
+      </div>
+    </section>
   )
 }
 
