f7cf63c8966f496acac6956ebe32899f7ab02b20
TheekshaRathnayaka <jrntheeksha@std.appsc.sab.ac.lk>
2024-11-07 23:14:15 +0530

    Add GetApp section heading

--- components/GetApp.tsx
+++ components/GetApp.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Button from './Button'
+import Image from 'next/image'
 
 const GetApp = () => {
   return (
@@ -6,6 +8,23 @@ const GetApp = () => {
 
       <div className="get-app">
 
+        <div className="z-20 flex w-full flex-1 flex-col items-start justify-center gap-12">
+
+          <h2 className="bold-40 lg:bold-64 xl:max-w-[320px]">Get for free now!</h2>
+          <p className="regular-16 text-gray-10">Available on iOS and Android</p>
+
+          <div className="flex w-full flex-col gap-3 whitespace-nowrap xl:flex-row">
+
+          <Button 
+              type="button"
+              title="App Store"
+              icon="/apple.svg"
+              variant="btn_white"
+              full
+            />
+            
+          </div>
+        </div>
       </div>
     </section>
   )
