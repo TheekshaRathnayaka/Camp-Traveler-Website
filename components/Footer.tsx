467ca6f68bd3e64a1b439ef905d879c5525a17d9
TheekshaRathnayaka <jrntheeksha@std.appsc.sab.ac.lk>
2024-11-11 23:39:32 +0530

    Add footer column

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,6 +8,7 @@ const Footer = () => {
   return (
     <footer className="flexCenter mb-24">
       <div className="padding-container max-container flex w-full flex-col gap-14">
+
         <div className="flex flex-col items-start justify-center gap-[10%] md:flex-row">
 
           <Link href="/" className="mb-10">
@@ -16,16 +17,40 @@ const Footer = () => {
 
           <div className='flex flex-wrap gap-10 sm:justify-between md:flex-1'>
             {FOOTER_LINKS.map((columns) => (
+
               <FooterColumn title={columns.title}>
+
                 <ul className="regular-14 flex flex-col gap-4 text-gray-30">
                   {columns.links.map((link) => (
+
                     <Link href="/" key={link}>
                       {link}
                     </Link>
+
                   ))}
                 </ul>
+
               </FooterColumn>
+
             ))}
+          </div>
+
+          <div className="flex flex-col gap-5">
+            <FooterColumn title={FOOTER_CONTACT_INFO.title}>
+              
+              {FOOTER_CONTACT_INFO.links.map((link) => (
+                <Link href="/" 
+                      key={link.label}
+                      className="flex gap-4 md:flex-col lg:flex-row">
+
+                  <p className="whitespace-nowrap">
+                    {link.label}:
+                  </p>
+                  
+                </Link>
+              ))}
+
+            </FooterColumn>
           </div>
 
         </div>
