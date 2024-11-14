9374db479ef9423d4074235003b8dd08e981c77f
TheekshaRathnayaka <jrntheeksha@std.appsc.sab.ac.lk>
2024-11-14 23:10:53 +0530

    Complete footer section

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -73,9 +73,13 @@ const Footer = () => {
               </ul>
             </FooterColumn>
           </div>
-
         </div>
       </div>
+
+      <div className="border bg-gray-20" >
+        <p className="regular-14 w-full text-center text-gray-30">2023 Hilink | All rights reserved</p>
+      </div>
+      
     </footer>
   )
 }
